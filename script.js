5ec58d837b241e9c002c930fa94df6cd008cbf04
Josephonate <131502920+Josephonate@users.noreply.github.com>
2023-07-17 22:57:27 -0400

    added functions into my JS

--- script.js
+++ script.js
@@ -10,8 +10,37 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
+const randomGen = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  char: getRandomChar
+};
+
+
+function getRandomLower() {
+  const lowerCase = "abcedefghijklmnopqrstuvwxyz";
+  return lowerCase[Math.floor(Math.random()* lowerCase.length)]
+}
+
+function getRandomUpper() {
+  const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  return upperCase[Math.floor(Math.random()* upperCaselength)]
+}
+
+function getRandomNumber() {
+  const numbers = "0123456789";
+  return numbers[Math.floor(Math.random()* numbers.length)]
+}
+
+function getRandomChar() {
+  const char = " !#$%&'\"()*+,-.\\/:;<=>?@[\]^_`{|}~";
+  return char[Math.floor(Math.random()* char.length)]
+}
+console.log(getRandomChar());
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
