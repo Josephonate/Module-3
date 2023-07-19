6e50013d1af56df0fa4c5c835eec0e3688f3d3d4
Josephonate <131502920+Josephonate@users.noreply.github.com>
2023-07-19 00:45:47 -0400

    added if statements and prompt

--- script.js
+++ script.js
@@ -1,45 +1,59 @@
 // Assignment code here
 
+// This is my random password gen criteria.
+function passwordGen(){
 
+var char = " !#$%&'\"()*+,-.\\/:;<=>?@[\]^_`{|}~";
+var numbers = "0123456789";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcedefghijklmnopqrstuvwxyz";
+var length = prompt("Please choose a length between 8 to 128 Characters");
+
+if (length < 8) {
+ alert ("Your password is too short try again!");
+}else if (length > 128) {
+ alert ("Your password is too long try again!");
+} else {
+ // return length;
+}
+
+if (confirm ("Do you want special characters in your Password?")) {
+ // return char;
+}
+
+if (confirm ("Do you want Uppercase letters in your Password?") ) {
+ // return upperCase;
+}
+if(confirm ("Do you want lowercase letters in your Password?")){
+ // return lowerCase;
+}
+if(confirm ("Do you want Numbers in your Password?")){
+ return length, char, upperCase, lowerCase, numbers;
+}
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  gen
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
-const randomGen = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  char: getRandomChar
-};
+function generatePassword() {
 
-
-function getRandomLower() {
-  const lowerCase = "abcedefghijklmnopqrstuvwxyz";
-  return lowerCase[Math.floor(Math.random()* lowerCase.length)]
+  var password = "";
+  
+ for(var i = 0; i < length; i++){
+  var randomChar = Math.floor(Math.random() * length);
+  password = password = randomChar;
+ }
+ return password;
 }
 
-function getRandomUpper() {
-  const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  return upperCase[Math.floor(Math.random()* upperCaselength)]
-}
-
-function getRandomNumber() {
-  const numbers = "0123456789";
-  return numbers[Math.floor(Math.random()* numbers.length)]
-}
-
-function getRandomChar() {
-  const char = " !#$%&'\"()*+,-.\\/:;<=>?@[\]^_`{|}~";
-  return char[Math.floor(Math.random()* char.length)]
-}
-console.log(getRandomChar());
 
 
 // Add event listener to generate button
